651707301de2c434e18bf0027a82cb5dee162654
dawg <dogleading1@gmail.com>
2025-01-26 15:19:04 +0200

    added the option to change the size of the board

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
+const defaultSize = 16;
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < defaultSize * defaultSize; i++) {
   const box = document.createElement("div");
   box.classList.add("item");
+  box.style.width = `${960 / defaultSize}px`;
+  box.style.height = `${960 / defaultSize}px`;
   container.appendChild(box);
 }
 
@@ -11,5 +14,31 @@ const items = document.querySelectorAll(".item");
 items.forEach((item) => {
   item.addEventListener("mouseover", () => {
     item.style.backgroundColor = "black";
+  });
+});
+
+const changeSize = document.querySelector(".change-size");
+
+changeSize.addEventListener("click", () => {
+  const newSize = prompt("Enter new size(1-150)");
+  if (newSize < 1 || newSize > 150) {
+    alert("Please enter a number between 1 and 150");
+    return;
+  }
+  container.innerHTML = "";
+
+  for (let i = 0; i < newSize * newSize; i++) {
+    const box = document.createElement("div");
+    box.classList.add("item");
+    box.style.width = `${960 / newSize}px`;
+    box.style.height = `${960 / newSize}px`;
+    container.appendChild(box);
+  }
+  const items = document.querySelectorAll(".item");
+
+  items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "black";
+    });
   });
 });
